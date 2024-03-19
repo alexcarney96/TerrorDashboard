8c1534196d105674dbe0000e0a4cff790b235ab8
alexcarney96 <160676516+alexcarney96@users.noreply.github.com>
2024-03-18 18:19:00 -0700

    stopped debug mode

--- DashboardCode/app.py
+++ DashboardCode/app.py
@@ -740,4 +740,4 @@ def update_active_links(pathname):
 
 if __name__ == '__main__':
     #port must match Dockerfile
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run_server(debug=False, host='0.0.0.0', port=8050)
